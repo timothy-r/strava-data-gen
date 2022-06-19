2e68f1e58ad879a9868fe1eaed0d9f70762f6a7d
timothy-r <tim.rodger@gmail.com>
2022-06-19 13:59:49 +0100

    Install venv & requirements

--- functions/handler.py
+++ functions/handler.py
@@ -1,5 +1,8 @@
 import datetime
 import logging
+import numpy as np
+
+from lib.AccessToken import AccessToken
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -9,3 +12,10 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
+    token = AccessToken()
+    logger.info("Access token {}".format(token))
+    
+    a = np.arange(15).reshape(3, 5)
+ 
+    logger.info("Your numpy array:")
+    logger.info(a)
